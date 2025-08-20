0e01e5ce5d0e3b523aeb92365311192b3e3991e8
shivraj130 <scholarshivraj@gmail.com>
2025-08-20 14:06:20 +0530

    Create script.js

---
+++ Experiment-4/script.js
@@ -0,0 +1,15 @@
+ const textInput = document.getElementById('text-input');
+        const charCount = document.getElementById('char-count');
+
+        textInput.addEventListener('input', () => {
+            const count = textInput.value.length;
+            charCount.textContent = count;
+        });
+
+        const textInput2 = document.getElementById('text-input-2');
+        const charCount2 = document.getElementById('char-count-2');
+
+        textInput2.addEventListener('input', () => {
+            const count = textInput2.value.length;
+            charCount2.textContent = count;
+        });
