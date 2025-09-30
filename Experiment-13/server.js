3b3dc04f8ba0adce3dddb9c2111bf17bf87fb160
shivraj130 <scholarshivraj@gmail.com>
2025-09-30 21:15:00 +0530

    Create server.js

---
+++ Experiment-13/server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/todos', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
